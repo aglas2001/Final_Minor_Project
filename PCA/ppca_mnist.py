445fb0a3e341b0de5236d97a5ff5c67be32cafd6
jobreijns <94359248+jobreijns@users.noreply.github.com>
2021-11-23 23:00:01 +0100

    Create ppca_mnist.py

---
+++ PCA/ppca_mnist.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 23 22:17:46 2021
+
+@author: jobre
+"""
+
+from sklearn.datasets import fetch_openml
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from matplotlib import pyplot as plt
+import numpy as np
+
+#%% importing dataset, takes a while...
+mnist = fetch_openml('mnist_784')
+
+data = mnist.data.to_numpy().astype('uint8')
+
+#%% ppca part
+
+scaler = StandardScaler()
+scaler.fit(data)
+
+# Applies standardization to both the training set and the test set.
+std_data = scaler.transform(data).astype('uint8')
+
+# ppca
+ppca = PCA(0.95)
+ppca.fit(std_data)
+reduced_dim = ppca.transform(std_data)
+
+print("dimensionality after pca:",reduced_dim.shape)
+
+
+# reconstructing the feature space with latent space:
+recon = ppca.inverse_transform(reduced_dim)
+recon = scaler.inverse_transform(recon)
+
+#%% plotting both original and reconstructed datasets
+labels = mnist.target.array
+plottable = np.reshape(data,(70000,28,28))
+rec_plottable = np.reshape(recon,(70000,28,28))
+
+for i in range(9):  
+    plt.subplot(1,2,1)
+    plt.title(labels[i])
+    plt.imshow(plottable[i], cmap = plt.get_cmap('gray'))
+    plt.subplot(1,2,2)
+    plt.imshow(rec_plottable[i], cmap=plt.get_cmap('gray'))
+    plt.title("reconstruction")
+    plt.show()
+
+
+
