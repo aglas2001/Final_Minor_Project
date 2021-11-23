164c2b81bdec69167724c5c23f25b34a1a38d257
jobreijns <94359248+jobreijns@users.noreply.github.com>
2021-11-23 23:01:24 +0100

    Update ppca_mnist.py

--- PCA/ppca_mnist.py
+++ PCA/ppca_mnist.py
@@ -16,7 +16,7 @@ import numpy as np
 #%% importing dataset, takes a while...
 mnist = fetch_openml('mnist_784')
 
-data = mnist.data.to_numpy().astype('uint8')
+data = mnist.data.to_numpy()
 
 #%% ppca part
 
@@ -24,14 +24,14 @@ scaler = StandardScaler()
 scaler.fit(data)
 
 # Applies standardization to both the training set and the test set.
-std_data = scaler.transform(data).astype('uint8')
+std_data = scaler.transform(data)
 
 # ppca
 ppca = PCA(0.95)
 ppca.fit(std_data)
 reduced_dim = ppca.transform(std_data)
 
-print("dimensionality after pca:",reduced_dim.shape)
+print("dimensionality after pca:",ppca.n_components_)
 
 
 # reconstructing the feature space with latent space:
@@ -40,7 +40,7 @@ recon = scaler.inverse_transform(recon)
 
 #%% plotting both original and reconstructed datasets
 labels = mnist.target.array
-plottable = np.reshape(data,(70000,28,28))
+plottable = np.reshape(data,(70000,28,28)).astype('uint8')
 rec_plottable = np.reshape(recon,(70000,28,28))
 
 for i in range(9):  
