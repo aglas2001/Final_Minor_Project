99aa2f6aeacda0918b5b0a5545453ab85a0ffcd8
jobreijns <94359248+jobreijns@users.noreply.github.com>
2021-12-08 17:32:00 +0100

    Update ICA_mnist.py

--- ICA/ICA_mnist.py
+++ ICA/ICA_mnist.py
@@ -13,6 +13,7 @@ from sklearn.linear_model import LogisticRegression
 from matplotlib import pyplot as plt
 import numpy as np
 from sklearn.decomposition import PCA
+import copy
 
 
 
@@ -24,6 +25,11 @@ from sklearn.decomposition import PCA
 #%% importing dataset, takes a while...
 mnist = fetch_openml('mnist_784')
 
+#%% extra data
+matr01 = np.zeros((2,784))
+for i in range(1,784):
+    matr01[1,i] = 1000
+matr01plot = np.reshape(matr01,(2,28,28))
 #%% Get data
 
 # These are the images
@@ -40,6 +46,9 @@ train_img, test_img, train_lbl, test_lbl = train_test_split(
 plottable = np.reshape(train_img.to_numpy(),(60000,28,28))
 labels = train_lbl.array
 
+plottest = np.reshape(test_img.to_numpy(),(10000,28,28))
+labelstest = test_lbl.array
+
 # Fit on training set only. Computes the mean and std to be used for later scaling.
 scaler = StandardScaler()
 scaler.fit(train_img)
@@ -51,8 +60,6 @@ test_img2 = scaler.transform(test_img)
 train_img3 = scaler.transform(train_img)
 test_img3 = scaler.transform(test_img)
 
-print('Starting Dimensionality:', train_img.shape)
-
 #%% PCA
 
 # Determine how much variance the pc should describe and apply pca:
@@ -60,71 +67,69 @@ pca = PCA(.95)
 pca.fit(train_img2)
 train_imgpca = pca.transform(train_img2)
 test_imgpca = pca.transform(test_img2)
-print("dimensionality after pca:",train_imgpca.shape)
 
 # reconstructing the feature space with latent space:
 reconpca = pca.inverse_transform(train_imgpca)
 reconpca = scaler.inverse_transform(reconpca)
 recpca_plottable = np.reshape(reconpca,(60000,28,28))
 
-#%% Other PCA
-#data = mnist.data.to_numpy()
 
-
-#scaler = StandardScaler()
-#scaler.fit(data)
-
-# Applies standardization to both the training set and the test set.
-#std_data = scaler.transform(data)
-otherpca = PCA(0.95)
-otherpca.fit(train_img2)
-reduced_dim = otherpca.transform(train_img2)
-
-print("dimensionality after other pca:",otherpca.n_components_)
-
-
-# reconstructing the feature space with latent space:
-recon = otherpca.inverse_transform(reduced_dim)
-recon = scaler.inverse_transform(recon)
-
-labelsppca = mnist.target.array
-plottable2 = np.reshape(train_img2,(60000,28,28)).astype('uint8')
-rec2_plottable = np.reshape(recon,(60000,28,28))
+recon_pca_test = pca.inverse_transform(test_imgpca)
+recon_pca_test = scaler.inverse_transform(recon_pca_test)
+rec_pca_test_plottable = np.reshape(recon_pca_test,(10000,28,28))
 
 
 #%%Applying ICA
 
 # # # Give the amount of components wanted and apply ICA:
-ica = FastICA(n_components=327,random_state=0, max_iter = 200)
+ica = FastICA(n_components=4,random_state=0, max_iter = 1000)
+
+
 ica.fit(train_img3)
 train_imgica = ica.transform(train_img3)
 test_imgica = ica.transform(test_img3)
 
+# train_imgica = ica.fit_transform(train_img3)
+# test_imgica = ica.fit_transform(test_img3)
 
-#train_transformed = ica.fit_transform(train_img3)
-#test_transformed = ica.fit_transform(test_img3)
-
-#print("Dimensionality after ICA:",train_imgica.shape)
-
-# # reconstructing the feature space with latent space:
+# reconstructing the feature space with latent space:
 reconica = ica.inverse_transform(train_imgica)
 reconica = scaler.inverse_transform(reconica)
 recica_plottable = np.reshape(reconica,(60000,28,28))
 
-# recontransform = ica.inverse_transform(train_transformed)
-# recontransform = scaler.inverse_transform(recontransform)
-# rectransform_plottable = np.reshape(recontransform,(60000,28,28))
+
+recon_ica_test = ica.inverse_transform(test_imgica)
+recon_ica_test = scaler.inverse_transform(recon_ica_test)
+rec_ica_test_plottable = np.reshape(recon_ica_test,(10000,28,28))
 
 
-#%% plotting a few examples:
-for i in range(5):
+A = ica.mixing_  # Get estimated mixing matrix
+
+#%% 01
+matr01ica = ica.transform(matr01)
+recon_matr01ica = ica.inverse_transform(matr01ica)
+recon_matr01ica = scaler.inverse_transform(recon_matr01ica)
+rec_ica_matr01ica_plottable = np.reshape(recon_matr01ica,(2,28,28))
+
+#%% Try find components
+
+print(test_imgica.shape)
+print(test_imgica[0])
+
+probeer = copy.copy(test_imgica)
+probeer[0,1] = 1
+print(probeer[0])
+
+recon_ica_test_probeer = ica.inverse_transform(probeer)
+recon_ica_test_probeer = scaler.inverse_transform(recon_ica_test_probeer)
+rec_ica_test_plottable_probeer = np.reshape(recon_ica_test_probeer,(10000,28,28))
+
+#%% plotting from train:
+print("Plotting Train")
+for i in range(10):
      print(i)
-     print(recpca_plottable[i].shape)
-     print(recica_plottable[i].shape)
-     print('PCA:', sum(sum(abs(plottable[i]-recpca_plottable[i]))))
-     print('ICA:', sum(sum(abs(plottable[i]-recica_plottable[i]))))
-#     print('PPCA:', sum(sum(abs(plottable[i]-rec2_plottable[i]))))
-     print('PCA-ICA:', sum(sum(abs(recica_plottable[i]-recpca_plottable[i]))))
+     print('PCA:', 1/784*sum(sum(abs(plottable[i]-recpca_plottable[i]))))
+     print('ICA:', 1/784*sum(sum(abs(plottable[i]-recica_plottable[i]))))
      plt.subplot(2,2,1)
      plt.title(labels[i])
      plt.imshow(plottable[i], cmap = plt.get_cmap('gray'))
@@ -134,9 +139,93 @@ for i in range(5):
      plt.subplot(2,2,3)
      plt.imshow(recica_plottable[i], cmap=plt.get_cmap('gray'))
      plt.title("ICA")
-#     plt.subplot(2,2,4)
-#     plt.imshow(rec2_plottable[i], cmap=plt.get_cmap('gray'))
-#     plt.title("PPCA")
      plt.show()
-    #%%
-print(labels)
+#%% Plotting from test
+print("Plotting Test")
+for i in range(1):
+    if labelstest[i] == '0':
+        print(i)
+        print('PCA:', 1/784*sum(sum(abs(plottest[i]-rec_pca_test_plottable[i]))))
+        print('ICA:', 1/784*sum(sum(abs(plottest[i]-rec_ica_test_plottable[i]))))
+        plt.subplot(2,2,1)
+        plt.title(labelstest[i])
+        plt.imshow(plottest[i], cmap = plt.get_cmap('gray'))
+        plt.subplot(2,2,2)
+        plt.imshow(rec_pca_test_plottable[i], cmap=plt.get_cmap('gray'))
+        plt.title("PCA")
+        plt.subplot(2,2,3)
+        plt.imshow(rec_ica_test_plottable[i], cmap=plt.get_cmap('gray'))
+        plt.title("ICA")
+        plt.show()
+     
+
+#%% Trying to find what components do
+
+
+for j in range(4):
+    Comp = np.reshape(A[:,j],(28,28))
+    plt.imshow(Comp, cmap = plt.get_cmap('gray'))
+    plt.show()
+    for i in range(5):
+        print(test_imgica[i])
+
+        comp0 = copy.copy(test_imgica)
+        comp0[i,j] = 0
+        print(comp0[i])
+
+        recon_ica_test_comp0 = ica.inverse_transform(comp0)
+        recon_ica_test_comp0 = scaler.inverse_transform(recon_ica_test_comp0)
+        rec_ica_test_plottable_comp0 = np.reshape(recon_ica_test_comp0,(10000,28,28))
+        
+        comp1 = copy.copy(test_imgica)
+        comp1[i,j] = 0.01
+        print(comp1[i])
+
+        recon_ica_test_comp1 = ica.inverse_transform(comp1)
+        recon_ica_test_comp1 = scaler.inverse_transform(recon_ica_test_comp1)
+        rec_ica_test_plottable_comp1 = np.reshape(recon_ica_test_comp1,(10000,28,28))    
+        
+        titlestr = "Changing Component " + str(j+1) + " for number " + str(labelstest[i])
+        fig, axs = plt.subplots(1, 4)
+        fig.suptitle(titlestr, fontsize=16)
+        axs[0].imshow(plottest[i], cmap = plt.get_cmap('gray'))
+        axs[0].set_title(labelstest[i])
+        
+        axs[1].imshow(rec_ica_test_plottable[i], cmap=plt.get_cmap('gray'))
+        axs[1].set_title("ICA recon")
+
+        axs[2].imshow(rec_ica_test_plottable_comp0[i], cmap=plt.get_cmap('gray'))
+        axs[2].set_title("Comp = 0")
+        axs[3].imshow(rec_ica_test_plottable_comp1[i], cmap=plt.get_cmap('gray'))
+        axs[3].set_title("Comp = 0.1")
+        plt.show()
+
+
+#%% Components of mixing matrix
+for i in range(4):
+    Comp = np.reshape(A[:,i],(28,28))
+    plt.imshow(Comp, cmap = plt.get_cmap('gray'))
+    plt.show()
+
+
+
+
+
+
+#%% Plotting from test
+print("Plotting 01")
+for i in range(2):
+    plt.subplot(2,2,1)
+    plt.title(i) 
+    plt.imshow(matr01plot[i], cmap = plt.get_cmap('gray'))
+    plt.subplot(2,2,2)
+    plt.imshow(rec_ica_matr01ica_plottable[i], cmap=plt.get_cmap('gray'))
+    plt.title("recon")
+    plt.show()
+    
+    
+
+
+
+
+
