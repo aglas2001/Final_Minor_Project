c4f1b8999aebaa99eab9a1bb2c9f1b4881992e94
jobreijns <94359248+jobreijns@users.noreply.github.com>
2021-12-14 17:34:24 +0100

    updated pca & ppca with sliders and representation of principal components

--- PCA/pca_mnist.py
+++ PCA/pca_mnist.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib.widgets import Slider, Button
 
 # Optional way to get mnist, but now it's type is tuple
 # from keras.datasets import mnist
@@ -19,46 +20,53 @@ import numpy as np
 
 
 #%% importing dataset, takes a while...
-mnist = fetch_openml('mnist_784')
+mnist_dataset = fetch_openml('mnist_784')
 
+#%% prepare data
+mnist = mnist_dataset.data.to_numpy()
 
-#%% processing data, applying pca and plotting original and reconstructed values
+scaler = StandardScaler()
 
-# These are the images
-mnist.data.shape
-      
-# These are the labels
-mnist.target.shape
+scaler.fit(mnist)
 
-# test_size: what proportion of original data is used for test set
-train_img, test_img, train_lbl, test_lbl = train_test_split(
-    mnist.data, mnist.target, test_size=1/7.0, random_state=0)
+#standardize data
+data = scaler.transform(mnist)
+
+N, D = data.shape
+mu = np.mean(data,axis=0)
+desired_dim = 4
+#%%
 
 #preserve orginial values train_img for plot later:
-plottable = np.reshape(train_img.to_numpy(),(60000,28,28))
-labels = train_lbl.array
+plottable = np.reshape(mnist,(N,28,28))
+labels = mnist_dataset.target.array
 
-# Fit on training set only. Computes the mean and std to be used for later scaling.
-scaler = StandardScaler()
-scaler.fit(train_img)
-
-# Applies standardization to both the training set and the test set.
-train_img = scaler.transform(train_img)
-test_img = scaler.transform(test_img)
 
 # Determine how much variance the pc should describe and apply pca:
-pca = PCA(n_components=10)
-pca.fit(train_img)
-train_img = pca.transform(train_img)
-test_img = pca.transform(test_img)
-print("dimensionality after pca:",train_img.shape)
+pca = PCA(n_components=desired_dim)
+pca.fit(data)
+latent = pca.transform(data)
+
+print("dimensionality in latent space:", latent.shape)
 
 # reconstructing the feature space with latent space:
-recon = pca.inverse_transform(train_img)
+recon = pca.inverse_transform(latent)
 recon = scaler.inverse_transform(recon)
-rec_plottable = np.reshape(recon,(60000,28,28))
+rec_plottable = np.reshape(recon,(N,28,28))
 
-# plotting a few examples:
+#%% plotting the principal components
+pc = pca.components_
+pc_plottable = np.reshape(pc,(desired_dim,28,28))
+
+for i in range(desired_dim):
+    plt.subplot(1,2,2)
+    plt.imshow(pc_plottable[i], cmap=plt.get_cmap('gray'))
+    plt.title("principal component ")
+    plt.show()
+    
+
+
+#%% plotting a few examples:
 for i in range(9):  
     plt.subplot(1,2,1)
     plt.title(labels[i])
@@ -68,24 +76,56 @@ for i in range(9):
     plt.title("reconstruction")
     plt.show()
 
+#%% manual reconstruction
+man_rec = np.dot(latent,pc)
+rec = scaler.inverse_transform(man_rec)
+man_rec_plottable = np.reshape(rec,(N,28,28))
+
+for i in range(9):  
+    plt.subplot(1,2,1)
+    plt.title("original reconstruction")
+    plt.imshow(rec_plottable[i], cmap = plt.get_cmap('gray'))
+    plt.subplot(1,2,2)
+    plt.imshow(man_rec_plottable[i], cmap=plt.get_cmap('gray'))
+    plt.title("manual reconstruction")
+    plt.show()
+
+#%% Slider tool
+def reconstruct(latent_var):
+    rec = scaler.inverse_transform(pca.inverse_transform(latent_var))
+    rec = np.reshape(rec,(28,28))
+    return rec;
+    
+
+def update_plot(val):
+        z_arr = np.array([z1_slider.val, z2_slider.val, z3_slider.val, z4_slider.val], dtype=np.single)
+        plot.set_data(reconstruct(z_arr))
+        fig.canvas.draw_idle()
+        
+        
+#%%
+
+fig = plt.figure()
+plot = plt.imshow(reconstruct(latent[0]), cmap='gist_gray')
+plt.subplots_adjust(bottom=0.4)
+plt.subplots_adjust(left=0.25, bottom=0.25)
+
+z_arr = latent[0]
+
+ax_z1 = plt.axes([0.25, 0.1, 0.65, 0.03]) 
+z1_slider = Slider(ax=ax_z1, label='Z1', valmin=-20, valmax=20, valinit=z_arr[0])
+ax_z2 = plt.axes([0.25, 0.15, 0.65, 0.03])
+z2_slider = Slider(ax=ax_z2, label='Z2', valmin=-20, valmax=20, valinit=z_arr[1])
+ax_z3 = plt.axes([0.25, 0.2, 0.65, 0.03])
+z3_slider = Slider(ax=ax_z3, label='Z3', valmin=-20, valmax=20, valinit=z_arr[2])
+ax_z4 = plt.axes([0.25, 0.25, 0.65, 0.03])
+z4_slider = Slider(ax=ax_z4, label='Z4', valmin=-20, valmax=20, valinit=z_arr[3])
+
+z1_slider.on_changed(update_plot)
+z2_slider.on_changed(update_plot)
+z3_slider.on_changed(update_plot)
+z4_slider.on_changed(update_plot)
 
 
-#%% potential machine learning:
-# # all parameters not specified are set to their defaults
-# # default solver is incredibly slow thats why we change it
-# # solver = 'lbfgs'
-# logisticRegr = LogisticRegression(solver = 'lbfgs')
 
-# # train your ML
-# logisticRegr.fit(train_img, train_lbl)
-
-# # Returns a NumPy Array
-# # Predict for One Observation (image)
-# logisticRegr.predict(test_img[0].reshape(1,-1))
-
-# # Predict for Multiple Observations (images) at Once
-# logisticRegr.predict(test_img[0:10])
-
-# score = logisticRegr.score(test_img, test_lbl)
-# print("accuracy score:",score)
 
--- PCA/ppca_mnist.py
+++ PCA/ppca_mnist.py
@@ -15,6 +15,7 @@ import scipy.sparse as sp
 import scipy.sparse.linalg as la
 import pandas as pd
 import time
+from matplotlib.widgets import Slider, Button
 
 def get_exp_z(W,sigma,M):
     exp_z = np.zeros((N,desired_dim))
@@ -65,6 +66,16 @@ def M_step(exp_z,M,prev_sig):
     sig_new = get_sigma(W_new,exp_z,M,prev_sig)
     return W_new,sig_new
 
+def reconstruct_1dim(Z):
+    recon = scaler.inverse_transform(np.transpose(np.dot(W,Z)))
+    rec = np.reshape(recon,(28,28))
+    return rec
+
+def update_plot(val):
+        z_arr = np.array([z1_slider.val, z2_slider.val, z3_slider.val, z4_slider.val], dtype=np.single)
+        plot.set_data(reconstruct_1dim(z_arr))
+        fig.canvas.draw_idle()
+
 #%% importing dataset, takes a while...
 mnist_dataset = fetch_openml('mnist_784')
 
@@ -80,14 +91,14 @@ data = scaler.transform(MNIST)
 
 N, D = data.shape
 mu = np.mean(data,axis=0)
-desired_dim = 10
+desired_dim = 4
 
 
 #%% ppca part
 # ppca
 
 old_W = np.random.rand(D,desired_dim)
-old_sig = 5
+old_sig = 2.1
 k = 0
 conv = 1000
 
@@ -102,13 +113,16 @@ while conv > 1e-4:
 
 end = time.time()
 
+W = old_W
+sig = old_sig
+
 print(end-start)
 
 
 
 #%% plotting both original and reconstructed datasets
 
-recon = scaler.inverse_transform(np.transpose(np.dot(old_W,np.transpose(z))))
+recon = scaler.inverse_transform(np.dot(z,np.transpose(W)))
 labels = mnist_dataset.target.array
 plottable = np.reshape(MNIST,(70000,28,28)).astype('uint8')
 rec_plottable = np.reshape(recon,(70000,28,28))
@@ -121,6 +135,54 @@ for i in range(9):
     plt.imshow(rec_plottable[i], cmap=plt.get_cmap('gray'))
     plt.title("reconstruction")
     plt.show()
+
+#%% plot the principal components:
+pc = scaler.inverse_transform(np.transpose(W))
+pc_plottable = np.reshape(pc,(desired_dim,28,28))
+fig, axs = plt.subplots(1, 4)
+
+for i in range(desired_dim):
+    #plt.subplot(1,2,2)
+    plt.imshow(pc_plottable[i], cmap=plt.get_cmap('gray'))
+    plt.title("principal components by ppca")
+    #plt.show()
+
+
+        #fig.suptitle(titlestr, fontsize=16)
+    axs[i].imshow(pc_plottable[i], cmap = plt.get_cmap('gray'))
+        #axs[i].set_title(labelstest[i])
+        
+        # axs[1].imshow(rec_ica_test_plottable[i], cmap=plt.get_cmap('gray'))
+        # axs[1].set_title("ICA recon")
+
+        # axs[2].imshow(rec_ica_test_plottable_comp0[i], cmap=plt.get_cmap('gray'))
+        # axs[2].set_title("Comp /= 10")
+        # axs[3].imshow(rec_ica_test_plottable_comp1[i], cmap=plt.get_cmap('gray'))
+        # axs[3].set_title("Comp *= 10")
+plt.show()
+
+#%% Slider:
+    
+fig = plt.figure()
+plot = plt.imshow(reconstruct_1dim(z[1]), cmap='gist_gray')
+plt.subplots_adjust(bottom=0.4)
+plt.subplots_adjust(left=0.25, bottom=0.25)
+
+z_arr = z[1]
+
+ax_z1 = plt.axes([0.25, 0.1, 0.65, 0.03]) 
+z1_slider = Slider(ax=ax_z1, label='Z1', valmin=-1, valmax=1, valinit=z_arr[0])
+ax_z2 = plt.axes([0.25, 0.15, 0.65, 0.03])
+z2_slider = Slider(ax=ax_z2, label='Z2', valmin=-1, valmax=1.1, valinit=z_arr[1])
+ax_z3 = plt.axes([0.25, 0.2, 0.65, 0.03])
+z3_slider = Slider(ax=ax_z3, label='Z3', valmin=-1, valmax=1, valinit=z_arr[2])
+ax_z4 = plt.axes([0.25, 0.25, 0.65, 0.03])
+z4_slider = Slider(ax=ax_z4, label='Z4', valmin=-1.1, valmax=1, valinit=z_arr[3])
+
+z1_slider.on_changed(update_plot)
+z2_slider.on_changed(update_plot)
+z3_slider.on_changed(update_plot)
+z4_slider.on_changed(update_plot)
 
 
 
