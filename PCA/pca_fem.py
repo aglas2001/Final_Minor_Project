393c86f9c10db7c7eb2f3e93d42cd9cdccc60d3c
jobreijns <94359248+jobreijns@users.noreply.github.com>
2021-12-15 22:11:00 +0100

    pca applied to nonlinear fem

--- PCA/pca_fem.py
+++ PCA/pca_fem.py
@@ -10,6 +10,9 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from os import listdir, walk
 from os.path import isfile, join
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
 
 
 #Custom Dataset loader for our displacment data
@@ -50,23 +53,48 @@ class DisplacementDataset(Dataset):
 #print(f"Displacements batch shape: {train_displacements_x.size()}")
 #print(f"Label: {train_displacements_y}")
 
+#%% load data, takes a while
+address = "../DataSet/Data_nonlinear/"
 
 
-folder_names = next(walk("../DataSet/Data_nonlinear/"), (None, None, []))[1]
-total_file_count = sum([len(files) for r, d, files in walk("../DataSet/Data_nonlinear/")])
+folder_names = next(walk(address), (None, None, []))[1]
+total_file_count = sum([len(files) for r, d, files in walk(address)])
 num_disp = 1034*2
 
 all_data = np.zeros((total_file_count,num_disp))
+count = 0
 
-for i in range(total_file_count):
-    temp = DisplacementDataset("../DataSet/Data_nonlinear/"+folder_names[i])
-    x,y = temp.__getitem__()
+for i in range(len(folder_names)):
+    temp = DisplacementDataset("../DataSet/Data_nonlinear/"+folder_names[i]).displacements
+    num_files = [len(files) for r, d, files in walk("../DataSet/Data_nonlinear/"+folder_names[i])][0]
+    
+    for j in range(num_files):
+        all_data[count] = temp[j,:]   
+        count += 1
 
 
 # print("../DataSet/Data_nonlinear/"+folder_names[1])
 
+#%% standardize data and set parameters
+scaler = StandardScaler()
 
+scaler.fit(all_data)
 
+#standardize data
+std_data = scaler.transform(all_data)
 
+N, D = std_data.shape
+mu = np.mean(std_data,axis=0)
+desired_dim = 10
 
+#%% apply pca
+pca = PCA(n_components=desired_dim)
+pca.fit(std_data)
+latent = pca.transform(std_data)
+
+#%% reconstuction
+
+rec_data = scaler.inverse_transform(pca.inverse_transform(latent))
+
+mean_squared_error(all_data, rec_data)
 
