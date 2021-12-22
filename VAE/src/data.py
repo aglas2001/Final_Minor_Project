cce52c40c635f6d97e768f43fe0dfefcb626dca1
Gijs Hooghiemstra <gijshooghiemstra@MacBook-Air-van-Gijs.local>
2021-12-22 12:53:47 +0100

    Changed the dataloader to be able to input displacement data. Started building a demo VAE.

--- VAE/src/data.py
+++ VAE/src/data.py
@@ -2,38 +2,72 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, isdir, join
 
 
 #Custom Dataset loader for our displacment data
 class DisplacementDataset(Dataset):
-    def __init__(self, data_folder):
-        file_count = len(listdir(data_folder))
+    def __init__(self, data_folder, ratio, seed, train):
 
-        displacement_matrix = np.zeros((file_count, 1034*2))
+        np.random.seed(seed)
 
-        def order(input):
-            return int(input.strip("para_.txt"))
+        file_list = list()
+        for folder in (listdir(data_folder)):
+            if isdir(join(data_folder, folder)):
+                for data_file in listdir(join(data_folder, folder)):
+                    if isfile(join(join(data_folder, folder), data_file)):
+                        file_list.append(join(join(data_folder, folder)))
 
-        count = 0
+        #train_folders = np.zeros((1-ratio)*len(folder_list))
+        #validation_folders = np.zeros(ratio*len(folder_list))
 
-        for data_file in sorted(listdir(data_folder), key=order):
-            if isfile(join(data_folder, data_file)):
-                data = np.loadtxt(join(data_folder, data_file), usecols=(0,1))
-                data = data.flatten()
-                displacement_matrix[count] = data
-                count += 1
-        self.displacements = displacement_matrix
-        print(self.displacements)
+        train_folders = np.random.choice(np.array(file_list), int((1-ratio)*len(file_list)), replace=False)
+        validation_folders = np.setxor1d(np.array(file_list), train_folders)
 
+        if train:
+            self.displacements = train_folders
+        else:
+            self.displacements = validation_folders
+
+
+        #def order(input):
+        #    return int(input.strip("para_.txt"))
+
+        #array_length = 0
+
+        # for folder in (listdir(data_folder)):
+        #     if isdir(join(data_folder, folder)):
+        #         for data_file in listdir(join(data_folder, folder)):
+        #             if isfile(join(join(data_folder, folder), data_file)):
+        #                 array_length += 1
+
+        # displacement_matrix = np.zeros((array_length, 1034*2))
+
+        # count = 0
+
+        #for folder in (listdir(data_folder)):
+        #    if isdir(join(data_folder, folder)):
+        #        file_count = len(listdir(data_folder))
+
+        #        sub_displacement_matrix = np.zeros((file_count, 1034*2))
+
+
+        #        for data_file in sorted(listdir(join(data_folder, folder)), key=order):
+        #            if isfile(join(join(data_folder, folder), data_file)):
+        #                data = np.loadtxt(join(join(data_folder, folder), data_file), usecols=(0,1))
+        #                data = data.flatten()
+        #                displacement_matrix[count] = data
+        #                count += 1
+
+        #self.displacements = np.array(displacement_matrix)
     def __len__(self):
         return len(self.displacements)
 
-    #def __getitem__(self, index):
-    #    displacement_x = self.displacements[index, 0]
-    #    displacement_y = self.displacements[index, 1]
-    #
-    #    return displacement_x, displacement_y
+    def __getitem__(self, index):
+        displacement = np.loadtxt(self.displacements[index], usecols=(0,1))
+        displacement = displacement.flatten()
+
+        return displacement
 
 #An example data loader using the custom dataset class
 #training_data = DisplacementDataset("../test_data/para_1.txt")
@@ -43,4 +77,4 @@ class DisplacementDataset(Dataset):
 #print(f"Displacements batch shape: {train_displacements_x.size()}")
 #print(f"Label: {train_displacements_y}")
 
-DisplacementDataset("../test_data/")
+DisplacementDataset("../../Dataset/Data_nonlinear/", ratio=0.2, seed=0, train=True)
