0d97061c4b7d620014438f264561f170d4848e6a
Gijs Hooghiemstra <gijshooghiemstra@MacBook-Air-van-Gijs.local>
2021-12-26 12:31:15 +0100

    Changed the dataloader to distribute timesteps of the same boundary condition.

--- VAE/src/data.py
+++ VAE/src/data.py
@@ -11,55 +11,34 @@ class DisplacementDataset(Dataset):
 
         np.random.seed(seed)
 
-        file_list = list()
+        folder_list = list()
         for folder in (listdir(data_folder)):
             if isdir(join(data_folder, folder)):
-                for data_file in listdir(join(data_folder, folder)):
-                    if isfile(join(join(data_folder, folder), data_file)):
-                        file_list.append(join(join(data_folder, folder), data_file))
+                folder_list.append(join(data_folder, folder))
 
-        #train_folders = np.zeros((1-ratio)*len(folder_list))
-        #validation_folders = np.zeros(ratio*len(folder_list))
+        train_folders = np.random.choice(np.array(folder_list), int((1-ratio)*len(folder_list)), replace=False)
+        validation_folders = np.setxor1d(np.array(folder_list), train_folders)
 
-        train_folders = np.random.choice(np.array(file_list), int((1-ratio)*len(file_list)), replace=False)
-        validation_folders = np.setxor1d(np.array(file_list), train_folders)
+        
 
         if train:
-            self.displacements = train_folders
+            train_data = list()
+
+            for folder in train_folders:
+                for data_file in listdir(join(data_folder, folder)):
+                    if isfile(join(join(data_folder, folder), data_file)):
+                        train_data.append(join(join(data_folder, folder), data_file))
+            self.displacements = np.array(train_data)
         else:
-            self.displacements = validation_folders
+            validation_data = list()
 
+            for folder in validation_folders:
+                for data_file in listdir(join(data_folder, folder)):
+                    if isfile(join(join(data_folder, folder), data_file)):
+                        validation_data.append(join(join(data_folder, folder), data_file))
+            self.displacements = np.array(validation_data)
 
-        #def order(input):
-        #    return int(input.strip("para_.txt"))
-
-        #array_length = 0
-
-        # for folder in (listdir(data_folder)):
-        #     if isdir(join(data_folder, folder)):
-        #         for data_file in listdir(join(data_folder, folder)):
-        #             if isfile(join(join(data_folder, folder), data_file)):
-        #                 array_length += 1
-
-        # displacement_matrix = np.zeros((array_length, 1034*2))
-
-        # count = 0
-
-        #for folder in (listdir(data_folder)):
-        #    if isdir(join(data_folder, folder)):
-        #        file_count = len(listdir(data_folder))
-
-        #        sub_displacement_matrix = np.zeros((file_count, 1034*2))
-
-
-        #        for data_file in sorted(listdir(join(data_folder, folder)), key=order):
-        #            if isfile(join(join(data_folder, folder), data_file)):
-        #                data = np.loadtxt(join(join(data_folder, folder), data_file), usecols=(0,1))
-        #                data = data.flatten()
-        #                displacement_matrix[count] = data
-        #                count += 1
-
-        #self.displacements = np.array(displacement_matrix)
+        print(len(self.displacements))
     def __len__(self):
         return len(self.displacements)
 
@@ -79,4 +58,4 @@ class DisplacementDataset(Dataset):
 #print(f"Displacements batch shape: {train_displacements_x.size()}")
 #print(f"Label: {train_displacements_y}")
 
-DisplacementDataset("../../Dataset/Data_nonlinear/", ratio=0.2, seed=0, train=True)
+DisplacementDataset("../../Dataset/Data_nonlinear/", ratio=0.2, seed=0, train=False)
